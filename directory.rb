cbb5c503f6991d1e54d35456766cf73273b188fe
D. Dramchev <ddramchev@gmail.com>
2022-08-05 11:00:15 +0100

    Add saving to file

--- directory.rb
+++ directory.rb
@@ -40,6 +40,8 @@ def interactive_menu
             input_students
         when "2"
             show_students
+        when "3"
+            save_students
         when "9"
             exit  # this will cause the program to terminate
         else
@@ -51,6 +53,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit"  # 9 because we'll be adding more items
 end
 
@@ -60,5 +63,16 @@ def show_students
     print_footer
 end
 
+def save_students
+    # open the file for writing
+    file = File.open("students.csv", "w")
+    # iterate over the array of statements
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
+end
 
 interactive_menu
